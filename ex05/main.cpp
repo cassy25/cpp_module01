bf955bc74f583a79c1e1ca98ac18bae4824bb026
Cassy <119490174+cassy25@users.noreply.github.com>
2024-01-31 17:17:50 +0100

    Create main.cpp

---
+++ ex05/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgeoffra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 13:23:55 by cgeoffra          #+#    #+#             */
+/*   Updated: 2023/11/27 13:23:56 by cgeoffra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    return 0;
+}
